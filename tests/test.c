9e3bd10275862a7916d4241372841304c7f2d111
zeb <bondzeb@gmail.com>
2026-03-19 10:48:31 +0000

    update

--- tests/test.c
+++ tests/test.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
-int main(int a) {
+int main(int a, char** argv) {
   int x = a;
   printf("i%i%i%i%i%i%i%i%i%i%i%i%i%i%i%i%i%i%i", 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-         1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, );
+         1, 1, 1, 1, 1, 1, 1, 1);
 }
